b3c8a5b6bb55c8cdbd343ce9092a309604b41d4a
stp <stp@dbc.dk>
2014-06-19 11:18:01 +0000

    Fixed #1195: Update: Opdatering til ny WSDL og posterne kan valideres og opdateres via de 2 nye EJB'ere
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@73287 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- service/updateservice-integration-tests/src/test/java/dk/dbc/updateservice/integration/tests/UpdateRecordIT.java
+++ service/updateservice-integration-tests/src/test/java/dk/dbc/updateservice/integration/tests/UpdateRecordIT.java
@@ -3,6 +3,11 @@ package dk.dbc.updateservice.integration.tests;
 
 //-----------------------------------------------------------------------------
 import dk.dbc.commons.jdbc.util.JDBCUtil;
+import dk.dbc.iscrum.records.MarcFactory;
+import dk.dbc.iscrum.records.MarcReader;
+import dk.dbc.iscrum.records.MarcRecord;
+import dk.dbc.rawrepo.RawRepoDAO;
+import dk.dbc.rawrepo.RecordId;
 import dk.dbc.updateservice.integration.BibliographicRecordFactory;
 import dk.dbc.updateservice.integration.UpdateServiceCaller;
 import dk.dbc.updateservice.integration.service.Options;
@@ -10,17 +15,24 @@ import dk.dbc.updateservice.integration.service.UpdateOptionEnum;
 import dk.dbc.updateservice.integration.service.UpdateRecordRequest;
 import dk.dbc.updateservice.integration.service.UpdateRecordResult;
 import dk.dbc.updateservice.integration.service.UpdateStatusEnum;
+import dk.dbc.updateservice.integration.service.ValidateEntry;
 import java.io.IOException;
+import java.io.StringReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import javax.ejb.EJBException;
 import javax.xml.parsers.ParserConfigurationException;
 import org.junit.After;
 import org.junit.AfterClass;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.slf4j.ext.XLogger;
+import org.slf4j.ext.XLoggerFactory;
 import org.xml.sax.SAXException;
 
 //-----------------------------------------------------------------------------
@@ -29,6 +41,12 @@ import org.xml.sax.SAXException;
  * @author stp
  */
 public class UpdateRecordIT {
+    private final XLogger logger = XLoggerFactory.getXLogger( this.getClass() );
+
+    private static final String BOOK_TEMPLATE_NAME = "us-book";
+    private static final String BOOK_MAIN_TEMPLATE_NAME = "us-bookmain";
+    private static final String BOOK_VOLUME_TEMPLATE_NAME = "us-bookvolume";
+    
     public UpdateRecordIT() {        
     }
     
@@ -57,26 +75,275 @@ public class UpdateRecordIT {
         }
     }
 
-    @Test
-    public void testValidateRecord() throws ParserConfigurationException, SAXException, IOException {
+    @Test( expected = javax.xml.ws.WebServiceException.class )
+    public void testRecordWithInvalidValidateSchema() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
         UpdateRecordRequest request = new UpdateRecordRequest();
         
         request.setAgencyId( "870970" );        
-        request.setValidateSchema( "bog" );
+        request.setValidateSchema( "Unknown-Schema" );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/record_validate_failure.xml" ) );
+
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+
+        assertNotNull( response );
+        assertEquals( UpdateStatusEnum.FAILED_INVALID_SCHEMA, response.getUpdateStatus() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+
+            assertFalse( rawRepo.recordExists( "1 234 567 8", 870970 ) );
+        }
+    }
+    
+    @Test
+    public void testRecordWithInvalidRecordSchema() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_TEMPLATE_NAME );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/record_validate_failure.xml" ) );
+        request.getBibliographicRecord().setRecordSchema( "Unknown-RecordSchema" );
+        
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+        
+        assertNotNull( response );
+        assertEquals( UpdateStatusEnum.FAILED_INVALID_SCHEMA, response.getUpdateStatus() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            assertFalse( rawRepo.recordExists( "1 234 567 8", 870970 ) );
+        }
+    }
+
+    @Test
+    public void testRecordWithInvalidRecordPacking() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_TEMPLATE_NAME );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/record_validate_failure.xml" ) );
+        request.getBibliographicRecord().setRecordPacking( "Unknown-RecordPacking" );
+        
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+        
+        assertNotNull( response );
+        assertEquals( UpdateStatusEnum.FAILED_INVALID_SCHEMA, response.getUpdateStatus() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            assertFalse( rawRepo.recordExists( "1 234 567 8", 870970 ) );
+        }
+    }
+
+    @Test
+    public void testValidateRecordWithFailure() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_TEMPLATE_NAME );
         Options options = new Options();
         options.getOption().add( UpdateOptionEnum.VALIDATE_ONLY );
         request.setOptions( options );
         request.setTrackingId( "trackingId" );
-        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "single_record.xml" ) );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/record_validate_failure.xml" ) );
         
         UpdateServiceCaller caller = new UpdateServiceCaller();
         UpdateRecordResult response = caller.updateRecord( request );
         
         assertNotNull( response );
         assertEquals( UpdateStatusEnum.VALIDATION_ERROR, response.getUpdateStatus() );
-        assertEquals( 0, response.getValidateInstance().getValidateEntry().size() );
+        assertNotSame( 0, response.getValidateInstance().getValidateEntry().size() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            assertFalse( rawRepo.recordExists( "1 234 567 8", 870970 ) );
+        }
+    }
+
+    @Test
+    public void testValidateRecordWithSuccess() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_TEMPLATE_NAME );
+        Options options = new Options();
+        options.getOption().add( UpdateOptionEnum.VALIDATE_ONLY );
+        request.setOptions( options );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/single_record.xml" ) );
+        
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+        
+        assertNotNull( response );
+        if( response.getValidateInstance() != null && response.getValidateInstance().getValidateEntry() != null && 
+            !response.getValidateInstance().getValidateEntry().isEmpty() ) 
+        {
+            ValidateEntry entry = response.getValidateInstance().getValidateEntry().get( 0 );
+            assertEquals( "", String.format( "%s: %s", entry.getOrdinalPositionOfField(), entry.getMessage() ) );
+        }
+        assertEquals( UpdateStatusEnum.VALIDATE_ONLY, response.getUpdateStatus() );
+        assertNull( response.getValidateInstance() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            assertFalse( rawRepo.recordExists( "20611529", 870970 ) );
+        }
+    }
+
+    @Test
+    public void testUpdateSingleRecordWithSuccess() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_TEMPLATE_NAME );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/single_record.xml" ) );
+        
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+        
+        assertNotNull( response );
+        if( response.getValidateInstance() != null && response.getValidateInstance().getValidateEntry() != null && 
+            !response.getValidateInstance().getValidateEntry().isEmpty() ) 
+        {
+            ValidateEntry entry = response.getValidateInstance().getValidateEntry().get( 0 );
+            assertEquals( "", String.format( "%s: %s", entry.getOrdinalPositionOfField(), entry.getMessage() ) );
+        }
+        assertEquals( UpdateStatusEnum.OK, response.getUpdateStatus() );
+        assertNull( response.getValidateInstance() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            String id = "20611529";
+            int library = 870970;
+            
+            assertTrue( rawRepo.recordExists( id, library ) );
+            String recContent = new String( rawRepo.fetchRecord( id, library ).getContent() );
+            List<MarcRecord> recs = new MarcFactory().createFromMarcXChange( new StringReader( recContent ) );
+            assertEquals( 1, recs.size() );
+            assertEquals( id, MarcReader.getRecordValue( recs.get( 0 ), "001", "a" ) );
+            assertEquals( String.valueOf( library ), MarcReader.getRecordValue( recs.get( 0 ), "001", "b" ) );
+        }
+    }
+
+    @Test
+    public void testUpdateVolumeRecordWithUnknownParent() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_VOLUME_TEMPLATE_NAME );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/volume_record.xml" ) );
+        
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+        
+        assertNotNull( response );
+        if( response.getValidateInstance() != null && response.getValidateInstance().getValidateEntry() != null && 
+            !response.getValidateInstance().getValidateEntry().isEmpty() ) 
+        {
+            ValidateEntry entry = response.getValidateInstance().getValidateEntry().get( 0 );
+            assertEquals( "", String.format( "%s: %s", entry.getOrdinalPositionOfField(), entry.getMessage() ) );
+        }
+        assertEquals( UpdateStatusEnum.FAILED_UPDATE_INTERNAL_ERROR, response.getUpdateStatus() );
+        assertNull( response.getValidateInstance() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            String id = "58442895";
+            int library = 870978;
+            
+            assertFalse( rawRepo.recordExists( id, library ) );
+        }
     }
     
+    @Test
+    public void testUpdateVolumeRecordWithValidParent() throws ParserConfigurationException, SAXException, IOException, ClassNotFoundException, SQLException {
+        UpdateRecordRequest request = new UpdateRecordRequest();
+        
+        // Update main record
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_MAIN_TEMPLATE_NAME );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/main_record.xml" ) );
+        
+        UpdateServiceCaller caller = new UpdateServiceCaller();
+        UpdateRecordResult response = caller.updateRecord( request );
+
+        // Check main record is updated.
+        assertNotNull( response );
+        if( response.getValidateInstance() != null && response.getValidateInstance().getValidateEntry() != null && 
+            !response.getValidateInstance().getValidateEntry().isEmpty() ) 
+        {
+            ValidateEntry entry = response.getValidateInstance().getValidateEntry().get( 0 );
+            assertEquals( "", String.format( "%s: %s", entry.getOrdinalPositionOfField(), entry.getMessage() ) );
+        }
+        assertEquals( UpdateStatusEnum.OK, response.getUpdateStatus() );
+        assertNull( response.getValidateInstance() );
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            assertTrue( rawRepo.recordExists( "58442615", 870978 ) );
+        }
+        
+        // Update volume record
+        request = new UpdateRecordRequest();
+        request.setAgencyId( "870970" );        
+        request.setValidateSchema( BOOK_VOLUME_TEMPLATE_NAME );
+        request.setOptions( new Options() );
+        request.setTrackingId( "trackingId" );
+        request.setBibliographicRecord( BibliographicRecordFactory.loadResource( "tests/volume_record.xml" ) );
+        
+        caller = new UpdateServiceCaller();
+        response = caller.updateRecord( request );
+
+        // Check volume record is updated.
+        assertNotNull( response );
+        if( response.getValidateInstance() != null && response.getValidateInstance().getValidateEntry() != null && 
+            !response.getValidateInstance().getValidateEntry().isEmpty() ) 
+        {
+            ValidateEntry entry = response.getValidateInstance().getValidateEntry().get( 0 );
+            assertEquals( "", String.format( "%s: %s", entry.getOrdinalPositionOfField(), entry.getMessage() ) );
+        }
+        assertEquals( UpdateStatusEnum.OK, response.getUpdateStatus() );
+        assertNull( response.getValidateInstance() );
+
+        try (final Connection connection = newRawRepoConnection()) {
+            final RawRepoDAO rawRepo = RawRepoDAO.newInstance( connection );
+            
+            assertTrue( rawRepo.recordExists( "58442615", 870978 ) );
+            assertTrue( rawRepo.recordExists( "58442895", 870978 ) );
+            
+            RecordId rawMainRecord = new RecordId( "58442615", 870978 );
+            Set<RecordId> rawVolumeSet = rawRepo.getRelationsChildren( rawMainRecord );
+            assertEquals( 1, rawVolumeSet.size() );
+            
+            Iterator<RecordId> iterator = rawVolumeSet.iterator();
+            RecordId volumeRec = iterator.next();
+            assertEquals( "58442895",volumeRec.getId() );
+            assertEquals( 870978,volumeRec.getLibrary() );
+        }
+    }
+
     private static Connection newRawRepoConnection() throws ClassNotFoundException, SQLException {
         Class.forName("org.postgresql.Driver");
         Connection conn = DriverManager.getConnection(
--- service/updateservice-ws/src/main/java/dk/dbc/updateservice/update/Updater.java
+++ service/updateservice-ws/src/main/java/dk/dbc/updateservice/update/Updater.java
@@ -6,19 +6,22 @@ import dk.dbc.iscrum.records.MarcReader;
 import dk.dbc.iscrum.records.MarcRecord;
 import dk.dbc.iscrum.records.MarcXchangeFactory;
 import dk.dbc.iscrum.records.marcxchange.CollectionType;
+import dk.dbc.iscrum.records.marcxchange.ObjectFactory;
 import dk.dbc.rawrepo.RawRepoDAO;
 import dk.dbc.rawrepo.Record;
 import dk.dbc.rawrepo.RecordId;
 import java.io.ByteArrayOutputStream;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.util.HashSet;
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
-import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
 import javax.sql.DataSource;
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import org.slf4j.ext.XLogger;
@@ -48,10 +51,14 @@ public class Updater {
     //              Business logic
     //-------------------------------------------------------------------------
 
-    public void updateRecord( MarcRecord record ) throws SQLException, ClassNotFoundException, JAXBException {
+    public void updateRecord( MarcRecord record ) throws SQLException, ClassNotFoundException, JAXBException, UpdateException {
         logger.entry( record );
         
         try( Connection conn = dataSource.getConnection() ) {
+            DatabaseMetaData metaData = conn.getMetaData();
+            logger.info( "Connection: Database product name: {}", metaData.getDatabaseProductName() );
+            logger.info( "Connection: JDBC driver: {} {}", metaData.getDriverName(), metaData.getDriverVersion() );
+            
             final RawRepoDAO rawRepoDAO = RawRepoDAO.newInstance( conn );
            
             byte[] content = encodeRecord( record );
@@ -70,21 +77,31 @@ public class Updater {
         logger.entry( record );
         
         MarcXchangeFactory marcXchangeFactory = new MarcXchangeFactory();
-        CollectionType marcXhangeCollectionType = marcXchangeFactory.createMarcXchangeFromMarc( record );
+        CollectionType marcXchangeCollectionType = marcXchangeFactory.createMarcXchangeFromMarc( record );
 
+        ObjectFactory objectFactory = new ObjectFactory();
+        JAXBElement<CollectionType> jAXBElement = objectFactory.createCollection( marcXchangeCollectionType );
+        
         JAXBContext jc = JAXBContext.newInstance( CollectionType.class );
         Marshaller marshaller = jc.createMarshaller();
         marshaller.setProperty( Marshaller.JAXB_SCHEMA_LOCATION, "http://www.loc.gov/standards/iso25577/marcxchange-2-0.xsd" );
         
         ByteArrayOutputStream recData = new ByteArrayOutputStream();
-        marshaller.marshal( marcXhangeCollectionType, recData );
+        marshaller.marshal( jAXBElement, recData );
         
         logger.exit( recData.toByteArray() );
         return recData.toByteArray();
     }
     
-    private void saveRecord( RawRepoDAO rawRepo, byte[] content, String recId, int libraryId, String parentId ) throws SQLException {
+    private void saveRecord( RawRepoDAO rawRepo, byte[] content, String recId, int libraryId, String parentId ) throws SQLException, UpdateException {
         logger.entry( rawRepo, content, recId, libraryId, parentId );
+        
+        if( !parentId.isEmpty() && !rawRepo.recordExists( parentId, libraryId ) ) {
+            String err = String.format( "Record [%s|%s] points to [%s|%s], but the referenced record does not exist in this rawrepo.", recId, libraryId, parentId, libraryId );
+            logger.warn( err );
+            throw new UpdateException( err );
+        }
+        
         
         final Record rawRepoRecord = rawRepo.fetchRecord( recId, libraryId );
         rawRepoRecord.setContent( content );
