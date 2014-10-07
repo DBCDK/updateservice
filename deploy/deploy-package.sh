2eefd3127b1654269243a736ee3949f86e5d4b64
stp <stp@dbc.dk>
2014-10-07 10:40:55 +0000

    Added README and deploy-package script.
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@77410 b87a6194-a221-0410-ad6f-3b8ec35bba13

---
+++ deploy/deploy-package.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p updateservice-1.0.0
+cd updateservice-1.0.0
+rm -rf *
+
+cp ../../README.md .
+cp ../../updateservice-system-tests/updateservice-system-tests-servers/glassfish/home/glassfish4/glassfish/domains/domain1/config/domain.xml .
+cp ../../updateservice-app/target/*.ear .
+cd ..
+
+tar -c -j updateservice-1.0.0 > updateservice-1.0.0.tgz
