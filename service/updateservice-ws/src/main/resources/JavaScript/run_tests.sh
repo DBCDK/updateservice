8de9ade68aa4c67a059bca48d0dc934b5884ff33
stp <stp@dbc.dk>
2014-10-07 07:27:52 +0000

    Moved code out of service folder into the root of the svn repository.
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@77354 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- service/updateservice-ws/src/main/resources/JavaScript/run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SHELL_CMD=dbc-jsshell
-SEARCH_PATH_ARGS="-a file:../iscrum -a file:../iscrum/templates -a file:../iscrum/rules -a file:. -l jstest.log -L info" 
-
-$SHELL_CMD $SEARCH_PATH_ARGS ClassificationData_UnitTests.js
