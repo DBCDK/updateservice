5f7647b853767312abf17de6d93ea2e85b8e784b
Thomas Hesselholt Lang <thl@dbc.dk>
2014-11-10 11:27:01 +0000

    Fjernet reference til fjernet metode
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@78841 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- updateservice-ws/src/test/java/dk/dbc/updateservice/javascript/JavaScriptTest.java
+++ updateservice-ws/src/test/java/dk/dbc/updateservice/javascript/JavaScriptTest.java
@@ -23,22 +23,22 @@ import dk.dbc.iscrumjs.ejb.JSEngine;
 //-----------------------------------------------------------------------------
 /**
  * Long description of this test suite...
- *  
+ *
  * @author stp
  */
 public class JavaScriptTest {
 	private static final XLogger logger = XLoggerFactory.getXLogger( JavaScriptTest.class );
 	private JSEngine jsEngine;
-	
+
     @BeforeClass
     public static void startWireMockServerAndLoadSettings() throws IOException {
         int serverPort = 12100;
         String serverRootDir = Paths.get( "." ).toFile().getCanonicalPath() + "/src/test/resources/wiremock/solr";
-        
+
         solrServer = new WireMockServer( wireMockConfig().port( serverPort ).withRootDirectory( serverRootDir ) );
         solrServer.start();
     }
-    
+
     @AfterClass
     public static void stopWireMockServers() {
         solrServer.stop();
@@ -51,17 +51,16 @@ public class JavaScriptTest {
 			Properties props = new Properties();
 			props.load( getClass().getResourceAsStream( "/dk/dbc/updateservice/javascript/settings.properties" ) );
 
-			jsEngine.init();
 			jsEngine.initialize( props );
         } catch ( IOException | IllegalArgumentException ex ) {
             logger.catching( XLogger.Level.WARN, ex );
         }
 	}
-	
+
 	@Test
 	public void test() throws Exception {
 		assertEquals( 0.0, jsEngine.callEntryPoint( "main" ) );
 	}
 
-    private static WireMockServer solrServer;	
+    private static WireMockServer solrServer;
 }
