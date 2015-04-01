4ca2f939fe5b64eebe52c01fb5e9e555799b610c
stp <stp@dbc.dk>
2015-04-01 12:53:21 +0000

    User story #1490: Udførelse af test imod lokal installation.
    Removed systems-tests from Maven. We wil use ocb-tests in stead.
    
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@83345 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- updateservice-system-tests/updateservice-system-tests-servers/glassfish/bin/undeploy-app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(cd "$(dirname "$0")"; pwd)/..
-GLASSFISH_HOME=$BASEDIR/home/glassfish4
-
-# Deploy application
-$GLASSFISH_HOME/bin/asadmin undeploy --port 13048 updateservice-app-1.0-SNAPSHOT
