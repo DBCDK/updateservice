40fb17b748b9419f6da5c4de4cd2ffcbbd7a91e6
stp <stp@dbc.dk>
2016-01-13 09:04:57 +0000

    Done some work on us #1763: Update: Docker-image til kørsel af stresstesten.
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@96066 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- stress-test/jmeter/run-tests.sh
+++ stress-test/jmeter/run-tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run -i -t --rm -v $(pwd):/root --add-host="service-host:$(HOST_IP)" -w /root hauptmedia/jmeter /opt/jmeter/bin/jmeter -n -t StressPlan.jmx
+docker run -i -t --rm -v $(pwd):/root --add-host="service-host:$HOST_IP" -w /root hauptmedia/jmeter /opt/jmeter/bin/jmeter -n -t StressPlan.jmx
