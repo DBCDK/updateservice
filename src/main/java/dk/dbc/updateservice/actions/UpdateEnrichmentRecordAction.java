0f292df1c68a3ef8b644cc1944e0927f514df90d
stp <stp@dbc.dk>
2016-03-14 12:02:46 +0000

    Commit some review changes from review #2339.
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@98118 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/actions/UpdateEnrichmentRecordAction.java
+++ src/main/java/dk/dbc/updateservice/actions/UpdateEnrichmentRecordAction.java
@@ -137,7 +137,7 @@ public class UpdateEnrichmentRecordAction extends AbstractRawRepoAction {
                 if( solrService.hasDocuments( SolrServiceIndexer.createSubfieldQuery( "002a", reader.recordId() ) ) ) {
                     String message = messages.getString( "update.record.with.002.links" );
 
-                    bizLogger.error( "Unable to create sub actions doing to an error: {}", message );
+                    bizLogger.error( "Unable to create sub actions due to an error: {}", message );
                     return ServiceResult.newErrorResult( UpdateStatusEnum.FAILED_UPDATE_INTERNAL_ERROR, message );
                 }
             }
