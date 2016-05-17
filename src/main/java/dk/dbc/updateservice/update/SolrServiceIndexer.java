9e64768e1971c04d0ade1afb9b31e7b8c1485c4f
Thomas Hesselholt Lang <thl@dbc.dk>
2016-05-17 11:34:31 +0000

    Pre-story formatting
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@100199 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
+++ src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
@@ -1,19 +1,13 @@
-//-----------------------------------------------------------------------------
 package dk.dbc.updateservice.update;
 
-//-----------------------------------------------------------------------------
 /**
  * This class has en responsibility to create query string to be used to make
  * lookup with the SolrService EJB.
  */
 public class SolrServiceIndexer {
-    public static String createSubfieldQueryDBCOnly( String fieldAndSubfield, String value ) {
-        return String.format( SUBFIELD_QUERY, fieldAndSubfield, value );
-    }
-
-    //-------------------------------------------------------------------------
-    //              Members
-    //-------------------------------------------------------------------------
-
     private static String SUBFIELD_QUERY = "marc.%s:\"%s\" AND marc.001b:870970";
+
+    public static String createSubfieldQueryDBCOnly(String fieldAndSubfield, String value) {
+        return String.format(SUBFIELD_QUERY, fieldAndSubfield, value);
+    }
 }
