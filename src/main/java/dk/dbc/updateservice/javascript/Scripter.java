d9254bae2a7df225d1de2bfe294045d888e2a6c2
Morten von Staffeldt <mvs@dbc.dk>
2016-06-02 11:57:08 +0000

    added log
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@100740 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/javascript/Scripter.java
+++ src/main/java/dk/dbc/updateservice/javascript/Scripter.java
@@ -54,7 +54,6 @@ public class Scripter {
             return result;
         }
         catch( Exception ex ) {
-            logger.error("mvs hest scripter #1");
             logger.error("message : " + ex.getMessage());
             logger.error(String.valueOf(ex));
             logger.catching(ex);
