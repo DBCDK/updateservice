d7dd47df632dcfa3163e169569e804caf1bd4a8f
Morten von Staffeldt <mvs@dbc.dk>
2016-06-21 08:54:30 +0000

    removed javascript files from updateService
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@101702 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/resources/JavaScript/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SHELL_CMD=dbc-jsshell
-SEARCH_PATH_ARGS="-a file:../iscrum/modules/unittests -a file:../iscrum -a file:../iscrum/templates -a file:../iscrum/rules -a file:. -l jstest.log -L info" 
-
-$SHELL_CMD $SEARCH_PATH_ARGS ClassificationData_UnitTests.js
-$SHELL_CMD $SEARCH_PATH_ARGS RawRepoCoreTests.js
