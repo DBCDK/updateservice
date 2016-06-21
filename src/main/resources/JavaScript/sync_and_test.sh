72fe68752c202e5f3f42ea98c855cb9defe3ecca
Morten von Staffeldt <mvs@dbc.dk>
2016-06-21 08:54:30 +0000

    removed javascript files from updateService
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@101702 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/resources/JavaScript/sync_and_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -av * stp@hejsa:/home/stp/projects/javascript/updateservice
-ssh hejsa -X "cd projects/javascript/updateservice; ./run_tests.sh"
-rsync -av stp@hejsa:/home/stp/projects/javascript/updateservice/*.log .
