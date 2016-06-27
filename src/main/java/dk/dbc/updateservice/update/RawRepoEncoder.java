b95d192fb2c35aec63cfc390beadeb3a57cf71a6
Thomas Hesselholt Lang <thl@dbc.dk>
2016-06-27 09:25:31 +0000

    Re-formatted file
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@101946 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/update/RawRepoEncoder.java
+++ src/main/java/dk/dbc/updateservice/update/RawRepoEncoder.java
@@ -22,6 +22,10 @@ import java.io.UnsupportedEncodingException;
  * Class to encode a MarcRecord as marcxchange xml for the rawrepo.
  */
 public class RawRepoEncoder {
+    private static final XLogger logger = XLoggerFactory.getXLogger(RawRepoEncoder.class);
+    private static final String SCHEMA_LOCATION = "http://www.loc.gov/standards/iso25577/marcxchange-1-1.xsd";
+    static final String ENCODING = "UTF-8";
+
     public RawRepoEncoder() {
     }
 
@@ -29,48 +33,37 @@ public class RawRepoEncoder {
      * Encodes the record as marcxchange.
      *
      * @param record The record to encode.
-     *
      * @return The encoded record as a sequence of bytes.
-     *
-     * @throws JAXBException if the record can not be encoded in marcxchange.
+     * @throws JAXBException                if the record can not be encoded in marcxchange.
      * @throws UnsupportedEncodingException if the record can not be encoded in UTF-8
      */
-    public byte[] encodeRecord( MarcRecord record ) throws JAXBException, UnsupportedEncodingException {
-        logger.entry( record );
+    public byte[] encodeRecord(MarcRecord record) throws JAXBException, UnsupportedEncodingException {
+        logger.entry(record);
         byte[] result = null;
 
         try {
-            if( record.getFields().isEmpty() ) {
+            if (record.getFields().isEmpty()) {
                 return null;
             }
 
-            dk.dbc.iscrum.records.marcxchange.RecordType marcXchangeType = MarcXchangeFactory.createMarcXchangeFromMarc( record );
+            dk.dbc.iscrum.records.marcxchange.RecordType marcXchangeType = MarcXchangeFactory.createMarcXchangeFromMarc(record);
 
             ObjectFactory objectFactory = new ObjectFactory();
-            JAXBElement<RecordType> jAXBElement = objectFactory.createRecord( marcXchangeType );
+            JAXBElement<RecordType> jAXBElement = objectFactory.createRecord(marcXchangeType);
 
-            JAXBContext jc = JAXBContext.newInstance( CollectionType.class );
+            JAXBContext jc = JAXBContext.newInstance(CollectionType.class);
             Marshaller marshaller = jc.createMarshaller();
-            marshaller.setProperty( Marshaller.JAXB_SCHEMA_LOCATION, SCHEMA_LOCATION );
+            marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, SCHEMA_LOCATION);
 
             StringWriter recData = new StringWriter();
-            marshaller.marshal( jAXBElement, recData );
+            marshaller.marshal(jAXBElement, recData);
 
-            logger.info( "Marshalled record: {}", recData.toString() );
-            result = recData.toString().getBytes( ENCODING );
+            logger.info("Marshalled record: {}", recData.toString());
+            result = recData.toString().getBytes(ENCODING);
 
             return result;
-        }
-        finally {
-            logger.exit( result );
+        } finally {
+            logger.exit(result);
         }
     }
-
-    //-------------------------------------------------------------------------
-    //              Members
-    //-------------------------------------------------------------------------
-
-    private static final XLogger logger = XLoggerFactory.getXLogger( RawRepoEncoder.class );
-    private static final String SCHEMA_LOCATION = "http://www.loc.gov/standards/iso25577/marcxchange-1-1.xsd";
-    static final String ENCODING = "UTF-8";
 }
