846eee8cfb7083e3d4ddd2e542f11230d5f11756
Anders Thrane Michelsen <atm@dbc.dk>
2016-09-26 09:07:08 +0000

    Story 1924 - Update: 002-solr opslag på hhv. *a og *b+*c
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@104438 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
+++ src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
@@ -6,8 +6,14 @@ package dk.dbc.updateservice.update;
  */
 public class SolrServiceIndexer {
     private static String SUBFIELD_QUERY = "marc.%s:\"%s\" AND marc.001b:870970";
+    private static String SUBFIELD_QUERY_DUAL = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND marc.001b:870970";
+
 
     public static String createSubfieldQueryDBCOnly(String fieldAndSubfield, String value) {
         return String.format(SUBFIELD_QUERY, fieldAndSubfield, value);
+    }
+
+    public static String createSubfieldQueryDualDBCOnly(String fieldAndSubfield1, String value1, String fieldAndSubfield2, String value2) {
+        return String.format(SUBFIELD_QUERY_DUAL, fieldAndSubfield1, value1, fieldAndSubfield2, value2);
     }
 }
