004a8102be51171f6328557b64f385d5c5622a00
Anders Thrane Michelsen <atm@dbc.dk>
2016-09-28 18:43:59 +0000

    Story 1924 - Forbedret logik, nu checkes der for duplicates UDEN at matche på den eksisternede record - Manglede fil...
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@104555 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
+++ src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
@@ -6,14 +6,25 @@ package dk.dbc.updateservice.update;
  */
 public class SolrServiceIndexer {
     private static String SUBFIELD_QUERY = "marc.%s:\"%s\" AND marc.001b:870970";
+    private static String SUBFIELD_QUERY_WITH_EXCLUDE = "marc.%s:\"%s\" AND !marc.%s:\"%s\" AND marc.001b:870970";
+
     private static String SUBFIELD_QUERY_DUAL = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND marc.001b:870970";
+    private static String SUBFIELD_QUERY_DUAL_WITH_EXCLUDE = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND !marc.%s:\"%s\" AND marc.001b:870970";
 
 
     public static String createSubfieldQueryDBCOnly(String fieldAndSubfield, String value) {
         return String.format(SUBFIELD_QUERY, fieldAndSubfield, value);
     }
 
+    public static String createSubfieldQueryWithExcludeDBCOnly(String fieldAndSubfield, String value, String fieldAndSubfieldExclude, String valueExclude) {
+        return String.format(SUBFIELD_QUERY_WITH_EXCLUDE, fieldAndSubfield, value, fieldAndSubfieldExclude, valueExclude);
+    }
+
     public static String createSubfieldQueryDualDBCOnly(String fieldAndSubfield1, String value1, String fieldAndSubfield2, String value2) {
         return String.format(SUBFIELD_QUERY_DUAL, fieldAndSubfield1, value1, fieldAndSubfield2, value2);
+    }
+
+    public static String createSubfieldQueryDualWithExcludeDBCOnly(String fieldAndSubfield1, String value1, String fieldAndSubfield2, String value2, String fieldAndSubfieldExclude, String valueExclude) {
+        return String.format(SUBFIELD_QUERY_DUAL_WITH_EXCLUDE, fieldAndSubfield1, value1, fieldAndSubfield2, value2, fieldAndSubfieldExclude, valueExclude);
     }
 }
