9d316611480c7f226353e5a7748111d6bdaaf4ab
Anders Thrane Michelsen <atm@dbc.dk>
2016-09-28 19:35:40 +0000

    Story 1924 - Nu burde systemtesten køre igennem
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@104556 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
+++ src/main/java/dk/dbc/updateservice/update/SolrServiceIndexer.java
@@ -6,10 +6,10 @@ package dk.dbc.updateservice.update;
  */
 public class SolrServiceIndexer {
     private static String SUBFIELD_QUERY = "marc.%s:\"%s\" AND marc.001b:870970";
-    private static String SUBFIELD_QUERY_WITH_EXCLUDE = "marc.%s:\"%s\" AND !marc.%s:\"%s\" AND marc.001b:870970";
+    private static String SUBFIELD_QUERY_WITH_EXCLUDE = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND marc.001b:870970";
 
     private static String SUBFIELD_QUERY_DUAL = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND marc.001b:870970";
-    private static String SUBFIELD_QUERY_DUAL_WITH_EXCLUDE = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND !marc.%s:\"%s\" AND marc.001b:870970";
+    private static String SUBFIELD_QUERY_DUAL_WITH_EXCLUDE = "marc.%s:\"%s\" AND marc.%s:\"%s\" AND marc.%s:\"%s\" AND marc.001b:870970";
 
 
     public static String createSubfieldQueryDBCOnly(String fieldAndSubfield, String value) {
@@ -17,7 +17,7 @@ public class SolrServiceIndexer {
     }
 
     public static String createSubfieldQueryWithExcludeDBCOnly(String fieldAndSubfield, String value, String fieldAndSubfieldExclude, String valueExclude) {
-        return String.format(SUBFIELD_QUERY_WITH_EXCLUDE, fieldAndSubfield, value, fieldAndSubfieldExclude, valueExclude);
+        return String.format(SUBFIELD_QUERY, fieldAndSubfield, value, fieldAndSubfieldExclude, valueExclude);
     }
 
     public static String createSubfieldQueryDualDBCOnly(String fieldAndSubfield1, String value1, String fieldAndSubfield2, String value2) {
@@ -25,6 +25,6 @@ public class SolrServiceIndexer {
     }
 
     public static String createSubfieldQueryDualWithExcludeDBCOnly(String fieldAndSubfield1, String value1, String fieldAndSubfield2, String value2, String fieldAndSubfieldExclude, String valueExclude) {
-        return String.format(SUBFIELD_QUERY_DUAL_WITH_EXCLUDE, fieldAndSubfield1, value1, fieldAndSubfield2, value2, fieldAndSubfieldExclude, valueExclude);
+        return String.format(SUBFIELD_QUERY_DUAL, fieldAndSubfield1, value1, fieldAndSubfield2, value2, fieldAndSubfieldExclude, valueExclude);
     }
 }
