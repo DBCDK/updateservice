119ee894f794e8fd9e0da1175d5b32adf796f6bb
Morten von Staffeldt <mvs@dbc.dk>
2016-12-05 10:29:57 +0000

    added isAllEnviromentsReady method for mesos use
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@107233 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/rest/StatusService.java
+++ src/main/java/dk/dbc/updateservice/rest/StatusService.java
@@ -12,11 +12,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import static javax.ws.rs.core.Response.Status.SERVICE_UNAVAILABLE;
+
 /**
  * REST webservice to invoke a very basic JavaScript.
  */
 @Stateless
-@Path("/status")
+@Path("/")
 public class StatusService {
     private static final XLogger logger = XLoggerFactory.getXLogger(StatusService.class);
 
@@ -25,13 +27,31 @@ public class StatusService {
     ScripterPool scripterPool;
 
     @GET
+    @Path("/status")
     @Produces(MediaType.TEXT_PLAIN)
-    public Response get() throws Exception {
+    public Response getStatus() throws Exception {
         logger.entry();
         String res = null;
         try {
             res = scripterPool.getStatus().toString();
             return Response.ok(res, MediaType.TEXT_PLAIN).build();
+        } finally {
+            logger.exit(res);
+        }
+    }
+
+    @GET
+    @Path("/isReady")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response isReady() throws Exception {
+        logger.entry();
+        boolean res = false;
+        try {
+            res = scripterPool.isAllEnviromentsLoaded();
+            if (res) {
+                return Response.ok("All enviroments are initialized").build();
+            }
+            return Response.status(SERVICE_UNAVAILABLE).build();
         } finally {
             logger.exit(res);
         }
