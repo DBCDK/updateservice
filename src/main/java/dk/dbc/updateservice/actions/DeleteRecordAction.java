bbaa6cbe3bdae34a7e55a68bb303a3f8af82be77
Anders Thrane Michelsen <atm@dbc.dk>
2017-01-05 11:36:09 +0000

    Bug 20696 - Ved sletning af FFU-poster via RR-maintenance slettes bind med __x ikke i corepo - Removed function
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@108363 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/main/java/dk/dbc/updateservice/actions/DeleteRecordAction.java
+++ src/main/java/dk/dbc/updateservice/actions/DeleteRecordAction.java
@@ -28,44 +28,6 @@ public class  DeleteRecordAction extends StoreRecordAction {
     }
 
     /**
-     * Note 2017-01-04: It has been decided that deletion of posts should not be filtered and instead the entire input
-     * record should be passed to rawrepo.
-     *
-     * Returns the record that should be stored in the rawrepo.
-     * <p>
-     * This implementation constructs a new record with the fields
-     * <code>001</code> and <code>004</code> from <code>this.record</code>.
-     * <code>004r</code> is overwritten with a <code>d</code> value.
-     * </p>
-     *
-     * @return The record to store.
-     */
-    //@Override
-    public MarcRecord recordToStoreDeprecated() {
-        logger.entry();
-        MarcRecord result = null;
-        try {
-            result = new MarcRecord();
-            for (MarcField field : record.getFields()) {
-                if (field.getName().equals("001")) {
-                    result.getFields().add(field);
-                }
-                if (field.getName().equals("004")) {
-                    result.getFields().add(field);
-                }
-                if (field.getName().equals("248")) {
-                    result.getFields().add(field);
-                }
-            }
-            MarcRecordWriter writer = new MarcRecordWriter(result);
-            writer.markForDeletion();
-            return result;
-        } finally {
-            logger.exit(result);
-        }
-    }
-
-    /**
      * Factory method to create a DeleteRecordAction.
      */
     public static DeleteRecordAction newDeleteRecordAction(GlobalActionState globalActionState,Properties properties, MarcRecord record, String mimetype) {
