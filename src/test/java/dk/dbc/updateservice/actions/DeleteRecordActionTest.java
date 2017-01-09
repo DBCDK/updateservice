b0e9285e32693f96ad89eacb848fe1e06c4afc1d
Anders Thrane Michelsen <atm@dbc.dk>
2017-01-09 16:58:30 +0000

    Story #2054 Update: håndtering af deleteposter med alene 001+004 - Added handling for records with only 001 and 004 fields
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@108553 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- src/test/java/dk/dbc/updateservice/actions/DeleteRecordActionTest.java
+++ src/test/java/dk/dbc/updateservice/actions/DeleteRecordActionTest.java
@@ -1,15 +1,24 @@
 package dk.dbc.updateservice.actions;
 
+import dk.dbc.iscrum.records.MarcField;
 import dk.dbc.iscrum.records.MarcRecord;
+import dk.dbc.iscrum.records.MarcRecordWriter;
+import dk.dbc.iscrum.records.MarcSubField;
 import dk.dbc.marcxmerge.MarcXChangeMimeType;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
 
 public class DeleteRecordActionTest {
     private GlobalActionState state;
@@ -44,6 +53,33 @@ public class DeleteRecordActionTest {
         DeleteRecordAction instance = new DeleteRecordAction(state, properties, record);
         instance.setMimetype(MarcXChangeMimeType.MARCXCHANGE);
         MarcRecord expected = AssertActionsUtil.loadRecord(AssertActionsUtil.LOCAL_SINGLE_RECORD_RESOURCE);
+        assertThat(instance.recordToStore(), equalTo(expected));
+    }
+
+    @Test
+    public void testRecordToStoreTwoFields() throws Exception {
+        String recordId = "20611529";
+        Integer agencyId = 700400;
+        List<MarcSubField> field001 = new ArrayList<>();
+        field001.add(new MarcSubField("a", recordId));
+        field001.add(new MarcSubField("b", Integer.toString(agencyId)));
+
+        List<MarcSubField> field004 = new ArrayList<>();
+        field001.add(new MarcSubField("r", "d"));
+
+        MarcRecord record = new MarcRecord();
+        record.getFields().add(new MarcField("001", "00", field001));
+        record.getFields().add(new MarcField("004", "00", field004));
+
+        MarcRecord rr = AssertActionsUtil.loadRecord(AssertActionsUtil.LOCAL_SINGLE_RECORD_RESOURCE);
+        when(state.getRawRepo().fetchRecord(eq(recordId), eq(agencyId))).thenReturn(AssertActionsUtil.createRawRepoRecord(rr, MarcXChangeMimeType.MARCXCHANGE));
+
+        DeleteRecordAction instance = new DeleteRecordAction(state, properties, record);
+        instance.setMimetype(MarcXChangeMimeType.MARCXCHANGE);
+
+        MarcRecord expected = AssertActionsUtil.loadRecord(AssertActionsUtil.LOCAL_SINGLE_RECORD_RESOURCE);
+        new MarcRecordWriter(expected).markForDeletion();
+
         assertThat(instance.recordToStore(), equalTo(expected));
     }
 }
