42bd6d1ff3484187d58a6734df470ed944711b7a
Michel Berggren <mib@dbc.dk>
2017-01-19 15:29:52 +0000

    The two system tests should now use private networks
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@109090 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- system-test.sh
+++ system-test.sh
@@ -5,7 +5,7 @@ function die() {
     exit 1
 }
 
-if [ $1 == "payara" ]; then
+if [ "$1" == "payara" ]; then
     echo "Running in payara mode: "
     SYSTEST_PATH="docker/deployments/systemtests-payara"
 else
@@ -13,6 +13,7 @@ else
     SYSTEST_PATH="docker/deployments/systemtests"
 fi
 
+COMPOSE_PROJECT_NAME=systemtests
 export HOST_IP=$(ip addr show | grep -A 99 '^2' | grep inet | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' |grep -v '^127.0.0.1' | head -1)
 echo "Using host IP: ${HOST_IP}"
 
@@ -45,10 +46,20 @@ docker-compose up -d update-systemtests-rawrepo-db \
                      update-systemtests-dataio  || die "docker-compose up -d update-systemtests-rawrepo-db \ update-systemtests-holdingsitems-db \ update-systemtests-update-db \  update-systemtests-fake-smtp \ update-systemtests-fbs \ update-systemtests-dataio"
 
 sleep 10 || die "sleep 10"
+docker tag docker-i.dbc.dk/mock-rawrepo-postgres:latest docker-i.dbc.dk/mock-rawrepo-postgres:systemtest
+docker rmi docker-i.dbc.dk/mock-rawrepo-postgres:latest
+docker tag docker-i.dbc.dk/mock-holdingsitems-postgres:latest docker-i.dbc.dk/mock-holdingsitems-postgres:systemtest
+docker rmi docker-i.dbc.dk/mock-holdingsitems-postgres:latest
 
+UPDATEDATAIO_IMAGE=`docker-compose ps -q update-systemtests-dataio`
+UPDATEDATAIO_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEDATAIO_IMAGE} `
+echo -e "UPDATEDATAIO_PORT_8080 is $UPDATEDATAIO_PORT_8080\n"
+UPDATEFBS_IMAGE=`docker-compose ps -q update-systemtests-fbs`
+UPDATEFBS_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEFBS_IMAGE} `
+echo -e "UPDATEFBS_PORT_8080 is $UPDATEFBS_PORT_8080\n"
 echo "Wait for glassfish containers"
-../../bin/return-when-status-ok.sh ${HOST_IP} 18180 '[dataio]' || die "could not start dataio"
-../../bin/return-when-status-ok.sh ${HOST_IP} 18280 '[fbs]' || die "could not start fbs"
+../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEDATAIO_PORT_8080} '[dataio]' || die "could not start dataio"
+../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEFBS_PORT_8080} '[fbs]' || die "could not start fbs"
 
 sleep 3 || die "sleep 3"
 
