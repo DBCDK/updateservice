e9050292f94c1b7f9743396f099fb63cab236eb5
Michel Berggren <mib@dbc.dk>
2017-01-23 15:30:58 +0000

    Better naming and less sleep
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@109262 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- system-test.sh
+++ system-test.sh
@@ -1,24 +1,5 @@
 #!/usr/bin/env bash
 
-function collect_logs () {
-
-   echo "Collect log files"
-   docker logs systemtests_update-systemtests-rawrepo-db_1 > logs/pg-rawrepo.log
-   docker logs systemtests_update-systemtests-holdingsitems-db_1 > logs/pg-holdingsitems.log
-   docker logs systemtests_update-systemtests-update-db_1 > logs/pg-updatedb.log
-   docker logs systemtests_update-systemtests-fbs_1 > logs/gf-fbs.log
-   docker logs systemtests_update-systemtests-dataio_1 > logs/gf-dataio.log
-   docker logs systemtests_ocb-tools-systemtests_1 > logs/ocb-tools.log
-
-}
-
-function die() {
-    echo "Error: $@ failed"
-    collect_logs
-    docker-compose down
-    exit 1
-}
-
 if [ "$1" == "payara" ]; then
     echo "Running in payara mode: "
     SYSTEST_PATH="docker/deployments/systemtests-payara"
@@ -28,6 +9,25 @@ else
     SYSTEST_PATH="docker/deployments/systemtests"
     export COMPOSE_PROJECT_NAME=systemtests
 fi
+
+function collect_logs () {
+
+   echo "Collect log files"
+   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-rawrepo-db_1 > logs/pg-rawrepo.log
+   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-holdingsitems-db_1 > logs/pg-holdingsitems.log
+   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-update-db_1 > logs/pg-updatedb.log
+   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-fbs_1 > logs/gf-fbs.log
+   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-dataio_1 > logs/gf-dataio.log
+   docker logs ${COMPOSE_PROJECT_NAME}_ocb-tools-systemtests_1 > logs/ocb-tools.log
+
+}
+
+function die() {
+    echo "Error: $@ failed"
+    collect_logs
+    docker-compose down
+    exit 1
+}
 
 export HOST_IP=$(ip addr show | grep -A 99 '^2' | grep inet | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' |grep -v '^127.0.0.1' | head -1)
 echo "Using host IP: ${HOST_IP}"
@@ -55,7 +55,6 @@ docker rmi 'docker-i.dbc.dk/update-postgres:candidate'
 docker rmi 'docker-i.dbc.dk/update-glassfish-deployer:candidate'
 docker rmi 'docker-i.dbc.dk/ocb-tools-deployer:latest'
 
-sleep 3 || die "sleep 3"
 echo "Startup glassfish containers here : `pwd`"
 docker-compose up -d update-systemtests-rawrepo-db \
                      update-systemtests-holdingsitems-db \
@@ -64,10 +63,9 @@ docker-compose up -d update-systemtests-rawrepo-db \
                      update-systemtests-fbs \
                      update-systemtests-dataio  || die "docker-compose up -d update-systemtests-rawrepo-db \ update-systemtests-holdingsitems-db \ update-systemtests-update-db \  update-systemtests-fake-smtp \ update-systemtests-fbs \ update-systemtests-dataio"
 
-sleep 10 || die "sleep 10"
-docker tag docker-i.dbc.dk/mock-rawrepo-postgres:latest docker-i.dbc.dk/mock-rawrepo-postgres:systemtest
+docker tag docker-i.dbc.dk/mock-rawrepo-postgres:latest docker-i.dbc.dk/mock-rawrepo-postgres:${COMPOSE_PROJECT_NAME}
 docker rmi docker-i.dbc.dk/mock-rawrepo-postgres:latest
-docker tag docker-i.dbc.dk/mock-holdingsitems-postgres:latest docker-i.dbc.dk/mock-holdingsitems-postgres:systemtest
+docker tag docker-i.dbc.dk/mock-holdingsitems-postgres:latest docker-i.dbc.dk/mock-holdingsitems-postgres:${COMPOSE_PROJECT_NAME}
 docker rmi docker-i.dbc.dk/mock-holdingsitems-postgres:latest
 
 UPDATEDATAIO_IMAGE=`docker-compose ps -q update-systemtests-dataio`
@@ -77,8 +75,9 @@ UPDATEFBS_IMAGE=`docker-compose ps -q update-systemtests-fbs`
 UPDATEFBS_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEFBS_IMAGE} `
 echo -e "UPDATEFBS_PORT_8080 is $UPDATEFBS_PORT_8080\n"
 echo "Wait for glassfish containers"
-../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEDATAIO_PORT_8080} 60 '[dataio]' || die "could not start dataio"
-../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEFBS_PORT_8080} 60 '[fbs]' || die "could not start fbs"
+
+../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEDATAIO_PORT_8080} 120 '[dataio]' || die "could not start dataio"
+../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEFBS_PORT_8080} 120 '[fbs]' || die "could not start fbs"
 
 sleep 3 || die "sleep 3"
 
