d0a15230e0623248faf13a3616e0e7c4816ecfaf
Morten von Staffeldt <mvs@dbc.dk>
2017-01-31 13:36:24 +0000

    added -glassfish where need be
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@109522 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- system-test.sh
+++ system-test.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
 if [ "$1" == "payara" ]; then
-    echo "Running in payara mode: "
+    echo "systest ---> Running in payara mode: "
     SYSTEST_PATH="docker/deployments/systemtests-payara"
     export COMPOSE_PROJECT_NAME=systemtestspayara
     export systest="-payara"
 else
-    echo "Running in glassfish mode: "
+    echo "systest ---> Running in glassfish mode: "
     SYSTEST_PATH="docker/deployments/systemtests"
     export COMPOSE_PROJECT_NAME=systemtests
     export systest="-glassfish"
 fi
 
 function collect_logs () {
-   echo "Collect log files"
+   echo "systest ---> Collect log files"
    docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-rawrepo-db${systest}_1 > logs/pg-rawrepo.log
    docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-holdingsitems-db${systest}_1 > logs/pg-holdingsitems.log
    docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-update-db${systest}_1 > logs/pg-updatedb.log
@@ -22,14 +22,14 @@ function collect_logs () {
    docker logs ${COMPOSE_PROJECT_NAME}_ocb-tools-systemtests_1 > logs/ocb-tools.log
 }
 function die() {
-    echo "Error: $@ failed"
+    echo "systest ---> Error: $@ failed"
     collect_logs
     docker-compose down
     exit 1
 }
 
 export HOST_IP=$(ip addr show | grep -A 99 '^2' | grep inet | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' |grep -v '^127.0.0.1' | head -1)
-echo "Using host IP: ${HOST_IP}"
+echo "systest ---> Using host IP: ${HOST_IP}"
 
 cd ${SYSTEST_PATH} || die "cd ${SYSTEST_PATH}"
 rm -rf logs  || die "rm -rf logs"
@@ -43,7 +43,7 @@ mkdir -p logs/ocb-tools || die "mkdir -p logs/ocb-tools"
 
 chmod -R a+rw logs || die "chmod -R a+rw logs"
 
-echo "Stop glassfish containers"
+echo "systest ---> Stop glassfish containers"
 docker-compose down
 
 #echo "Removing old images"
@@ -77,22 +77,22 @@ docker rmi docker-i.dbc.dk/mock-holdingsitems-postgres:latest
 
 UPDATEDATAIO_IMAGE=`docker-compose ps -q update-systemtests-dataio`
 UPDATEDATAIO_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEDATAIO_IMAGE} `
-echo -e "UPDATEDATAIO_PORT_8080 is $UPDATEDATAIO_PORT_8080\n"
+echo -e "systest ---> UPDATEDATAIO_PORT_8080 is $UPDATEDATAIO_PORT_8080\n"
 UPDATEFBS_IMAGE=`docker-compose ps -q update-systemtests-fbs`
 UPDATEFBS_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEFBS_IMAGE} `
-echo -e "UPDATEFBS_PORT_8080 is $UPDATEFBS_PORT_8080\n"
-echo "Wait for glassfish containers"
+echo -e "systest ---> UPDATEFBS_PORT_8080 is $UPDATEFBS_PORT_8080\n"
+echo "systest ---> Wait for glassfish containers"
 
 ../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEDATAIO_PORT_8080} 220 '[dataio]' || die "could not start dataio"
 ../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEFBS_PORT_8080} 220 '[fbs]' || die "could not start fbs"
 
 sleep 3 || die "sleep 3"
 
-echo "Start and run systemtests"
+echo "systest ---> Start and run systemtests"
 docker-compose up ocb-tools-systemtests || die "docker-compose up ocb-tools-systemtests"
 
 collect_logs
 
 sleep 3 || die "sleep 3"
-echo "Stop glassfish containers"
+echo "systest ---> Stop glassfish containers"
 docker-compose down || die "docker-compose down"
