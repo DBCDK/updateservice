607603e66e857167fcd868cc1383a9f13bbf764f
Morten von Staffeldt <mvs@dbc.dk>
2017-02-02 15:35:55 +0000

    cleaned up systest in regards to fbs and dataio being merged
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@109640 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- system-test.sh
+++ system-test.sh
@@ -11,8 +11,7 @@ function collect_logs () {
   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-rawrepo-db${systest}_1 > logs/pg-rawrepo.log
   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-holdingsitems-db${systest}_1 > logs/pg-holdingsitems.log
   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-update-db${systest}_1 > logs/pg-updatedb.log
-  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-fbs_1 > logs/gf-fbs.log
-  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-dataio_1 > logs/gf-dataio.log
+  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-updateservice_1 > logs/gf-updateservice.log
   docker logs ${COMPOSE_PROJECT_NAME}_ocb-tools-systemtests_1 > logs/ocb-tools.log
 }
 
@@ -24,8 +23,7 @@ function removeImages() {
   docker rmi 'docker-i.dbc.dk/fakesmtp:latest'
   docker rmi 'docker-i.dbc.dk/ocb-tools-deployer:latest'
   
-  docker rmi 'docker-i.dbc.dk/update-fbs:candidate'
-  docker rmi 'docker-i.dbc.dk/update-dataio:candidate'
+  docker rmi 'docker-i.dbc.dk/update-updateservice:candidate'
 }
 
 function startContainers () {
@@ -33,8 +31,7 @@ function startContainers () {
   docker-compose up -d update-systemtests-holdingsitems-db${systest}                 || die "docker-compose up -d update-systemtests-holdingsitems-db${systest}"
   docker-compose up -d update-systemtests-update-db${systest}                        || die "docker-compose up -d update-systemtests-update-db${systest}"
   docker-compose up -d update-systemtests-fake-smtp                                  || die  "docker-compose up -d update-systemtests-fake-smtp"
-  docker-compose up -d update-systemtests-dataio                                     || die  "docker-compose up -d update-systemtests-dataio"
-  docker-compose up -d update-systemtests-fbs                                        || die  "docker-compose up -d update-systemtests-fbs"
+  docker-compose up -d update-systemtests-updateservice                                     || die  "docker-compose up -d update-systemtests-updateservice"
 }
 
 function reTagAndRemove () {
@@ -47,25 +44,19 @@ function reTagAndRemove () {
 function setupLogAndLogdir () {
   cd ${SYSTEST_PATH} || die "cd ${SYSTEST_PATH}"
   rm -rf logs  || die "rm -rf logs"
-  mkdir -p logs/dataio || die "mkdir -p logs/dataio"
-  mkdir -p logs/fbs || die "mkdir -p logs/fbs"
-  cp ../../../update-logback-include.xml logs/dataio/  || die "cp ../../../update-logback-include.xml logs/dataio/"
-  cp ../../../update-logback-include.xml logs/fbs/ || die "cp ../../../update-logback-include.xml logs/fbs/"
+  mkdir -p logs/updateservice || die "mkdir -p logs/updateservice"
+  cp ../../../update-logback-include.xml logs/updateservice/  || die "cp ../../../update-logback-include.xml logs/updateservice/"
   mkdir -p logs/ocb-tools || die "mkdir -p logs/ocb-tools"
   chmod -R a+rw logs || die "chmod -R a+rw logs"
 }
 
 function waitForOk () {
-  UPDATEDATAIO_IMAGE=`docker-compose ps -q update-systemtests-dataio`
-  UPDATEDATAIO_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEDATAIO_IMAGE} `
-  echo -e "systest ---> UPDATEDATAIO_PORT_8080 is $UPDATEDATAIO_PORT_8080\n"
-  UPDATEFBS_IMAGE=`docker-compose ps -q update-systemtests-fbs`
-  UPDATEFBS_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEFBS_IMAGE} `
-  echo -e "systest ---> UPDATEFBS_PORT_8080 is $UPDATEFBS_PORT_8080\n"
+  UPDATE_SERVICE_IMAGE=`docker-compose ps -q update-systemtests-updateservice`
+  UPDATESERVICE_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATE_SERVICE_IMAGE} `
+  echo -e "systest ---> UPDATESERVICE_PORT_8080 is $UPDATESERVICE_PORT_8080\n"
   echo "systest ---> Wait for glassfish containers"
   
-  ../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEDATAIO_PORT_8080} 220 '[dataio]' || die "could not start dataio"
-  ../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEFBS_PORT_8080} 220 '[fbs]' || die "could not start fbs"
+  ../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATESERVICE_PORT_8080} 220 '[updateservice]' || die "could not start updateservice"
 }
 
 function setSysVars () {
