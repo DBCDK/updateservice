955a8903000f4c766e2f9a0d0245e51f0d2a4cbe
Anders Thrane Michelsen <atm@dbc.dk>
2017-02-02 12:12:26 +0000

    Story #2056 - Update: Slå dataio og fbs sammen - fix build
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@109620 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- system-test.sh
+++ system-test.sh
@@ -17,8 +17,7 @@ function collect_logs () {
    docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-rawrepo-db${systest}_1 > logs/pg-rawrepo.log
    docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-holdingsitems-db${systest}_1 > logs/pg-holdingsitems.log
    docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-update-db${systest}_1 > logs/pg-updatedb.log
-   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-fbs_1 > logs/gf-fbs.log
-   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-dataio_1 > logs/gf-dataio.log
+   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-updateservice_1 > logs/gf-fbs.log
    docker logs ${COMPOSE_PROJECT_NAME}_ocb-tools-systemtests_1 > logs/ocb-tools.log
 }
 function die() {
@@ -34,10 +33,9 @@ echo "systest ---> Using host IP: ${HOST_IP}"
 cd ${SYSTEST_PATH} || die "cd ${SYSTEST_PATH}"
 rm -rf logs  || die "rm -rf logs"
 
-mkdir -p logs/dataio || die "mkdir -p logs/dataio"
-mkdir -p logs/fbs || die "mkdir -p logs/fbs"
-cp ../../../update-logback-include.xml logs/dataio/  || die "cp ../../../update-logback-include.xml logs/dataio/"
-cp ../../../update-logback-include.xml logs/fbs/ || die "cp ../../../update-logback-include.xml logs/fbs/"
+mkdir -p logs/update || die "mkdir -p logs/update"
+cp ../../../update-logback-include.xml logs/update/  || die "cp ../../../update-logback-include.xml logs/update/"
+
 
 mkdir -p logs/ocb-tools || die "mkdir -p logs/ocb-tools"
 
@@ -59,8 +57,7 @@ docker-compose up -d update-systemtests-rawrepo-db${systest} \
                      update-systemtests-holdingsitems-db${systest} \
                      update-systemtests-update-db${systest} \
                      update-systemtests-fake-smtp \
-                     update-systemtests-fbs \
-                     update-systemtests-dataio  || die "docker-compose up -d update-systemtests-rawrepo-db \ update-systemtests-holdingsitems-db \ update-systemtests-update-db \  update-systemtests-fake-smtp \ update-systemtests-fbs \ update-systemtests-dataio"
+                     update-systemtests-updateservice  || die "docker-compose up -d update-systemtests-rawrepo-db \ update-systemtests-holdingsitems-db \ update-systemtests-update-db \  update-systemtests-fake-smtp \ update-systemtests-fbs \ update-systemtests-dataio"
 
 #echo "Startup glassfish containers here : `pwd`"
 #docker-compose up -d update-systemtests-rawrepo-db${systest} || die "rawrepo"
@@ -75,16 +72,11 @@ docker rmi docker-i.dbc.dk/mock-rawrepo-postgres:latest
 docker tag docker-i.dbc.dk/mock-holdingsitems-postgres:latest docker-i.dbc.dk/mock-holdingsitems-postgres:${COMPOSE_PROJECT_NAME}
 docker rmi docker-i.dbc.dk/mock-holdingsitems-postgres:latest
 
-UPDATEDATAIO_IMAGE=`docker-compose ps -q update-systemtests-dataio`
-UPDATEDATAIO_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEDATAIO_IMAGE} `
-echo -e "systest ---> UPDATEDATAIO_PORT_8080 is $UPDATEDATAIO_PORT_8080\n"
-UPDATEFBS_IMAGE=`docker-compose ps -q update-systemtests-fbs`
-UPDATEFBS_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATEFBS_IMAGE} `
-echo -e "systest ---> UPDATEFBS_PORT_8080 is $UPDATEFBS_PORT_8080\n"
-echo "systest ---> Wait for glassfish containers"
+UPDATESERVICE_IMAGE=`docker-compose ps -q update-systemtests-updateservice`
+UPDATESERVICE_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATESERVICE_IMAGE} `
+echo -e "systest ---> UPDATESERVICE_PORT_8080 is $UPDATESERVICE_PORT_8080\n"
 
-../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEDATAIO_PORT_8080} 220 '[dataio]' || die "could not start dataio"
-../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATEFBS_PORT_8080} 220 '[fbs]' || die "could not start fbs"
+../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATESERVICE_PORT_8080} 220 '[updateservice]' || die "could not start updateservice"
 
 sleep 3 || die "sleep 3"
 
