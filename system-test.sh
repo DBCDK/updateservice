b65095ec37f8c3c4f3eff197a3046acb6892e6e2
Morten von Staffeldt <mvs@dbc.dk>
2017-02-21 14:28:28 +0000

    cleaned up unused system tests
    git-svn-id: https://svn.dbc.dk/repos/updateservice/trunk@110337 b87a6194-a221-0410-ad6f-3b8ec35bba13

--- system-test.sh
+++ system-test.sh
@@ -8,31 +8,35 @@ function die() {
 }
 
 function collect_logs () {
-  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-rawrepo-db${systest}_1 > logs/pg-rawrepo.log
-  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-holdingsitems-db${systest}_1 > logs/pg-holdingsitems.log
-  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-update-db${systest}_1 > logs/pg-updatedb.log
+  echo "systest ---> Collect log files"
+  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-rawrepo-db_1 > logs/pg-rawrepo.log
+  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-holdingsitems-db_1 > logs/pg-holdingsitems.log
+  docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-update-db_1 > logs/pg-updatedb.log
   docker logs ${COMPOSE_PROJECT_NAME}_update-systemtests-updateservice_1 > logs/gf-updateservice.log
   docker logs ${COMPOSE_PROJECT_NAME}_ocb-tools-systemtests_1 > logs/ocb-tools.log
 }
 
 function removeImages() {
-  docker rmi 'docker-i.dbc.dk/mock-rawrepo-postgres'${systest}':latest'
-  docker rmi 'docker-i.dbc.dk/mock-holdingsitems-postgres'${systest}':latest'
-  docker rmi 'docker-i.dbc.dk/update-postgres'${systest}':candidate'
+  echo "systest ---> Removing old images"
+  docker rmi 'docker-i.dbc.dk/mock-rawrepo-postgres:latest'
+  docker rmi 'docker-i.dbc.dk/mock-holdingsitems-postgres:latest'
+  docker rmi 'docker-i.dbc.dk/update-postgres:candidate'
   docker rmi 'docker-i.dbc.dk/fakesmtp:latest'
   docker rmi 'docker-i.dbc.dk/ocb-tools-deployer:latest'
   docker rmi 'docker-i.dbc.dk/update-updateservice:candidate'
 }
 
 function startContainers () {
-  docker-compose up -d update-systemtests-rawrepo-db${systest}  || die "docker-compose up -d update-systemtests-rawrepo-db${systest}"
-  docker-compose up -d update-systemtests-holdingsitems-db${systest}                 || die "docker-compose up -d update-systemtests-holdingsitems-db${systest}"
-  docker-compose up -d update-systemtests-update-db${systest}                        || die "docker-compose up -d update-systemtests-update-db${systest}"
+  echo "systest ---> Starting containers"
+  docker-compose up -d update-systemtests-rawrepo-db  || die "docker-compose up -d update-systemtests-rawrepo-db"
+  docker-compose up -d update-systemtests-holdingsitems-db                 || die "docker-compose up -d update-systemtests-holdingsitems-db"
+  docker-compose up -d update-systemtests-update-db                        || die "docker-compose up -d update-systemtests-update-db"
   docker-compose up -d update-systemtests-fake-smtp                                  || die  "docker-compose up -d update-systemtests-fake-smtp"
   docker-compose up -d update-systemtests-updateservice                                     || die  "docker-compose up -d update-systemtests-updateservice"
 }
 
 function reTagAndRemove () {
+  echo "systest ---> retagging and removing containers"
   docker tag docker-i.dbc.dk/mock-rawrepo-postgres:latest docker-i.dbc.dk/mock-rawrepo-postgres:${COMPOSE_PROJECT_NAME}
   docker rmi docker-i.dbc.dk/mock-rawrepo-postgres:latest
   docker tag docker-i.dbc.dk/mock-holdingsitems-postgres:latest docker-i.dbc.dk/mock-holdingsitems-postgres:${COMPOSE_PROJECT_NAME}
@@ -40,6 +44,7 @@ function reTagAndRemove () {
 }
 
 function setupLogAndLogdir () {
+  echo "systest ---> Setting up logdirs"
   cd ${SYSTEST_PATH} || die "cd ${SYSTEST_PATH}"
   rm -rf logs  || die "rm -rf logs"
   mkdir -p logs/updateservice || die "mkdir -p logs/updateservice"
@@ -51,50 +56,34 @@ function setupLogAndLogdir () {
 }
 
 function waitForOk () {
+  echo "systest ---> waiting on containers"
   UPDATE_SERVICE_IMAGE=`docker-compose ps -q update-systemtests-updateservice`
   UPDATESERVICE_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATE_SERVICE_IMAGE} `
   echo -e "systest ---> UPDATESERVICE_PORT_8080 is $UPDATESERVICE_PORT_8080\n"
   echo "systest ---> Wait for glassfish containers"
-  
   ../../bin/return-when-status-ok.sh ${HOST_IP} ${UPDATESERVICE_PORT_8080} 220 '[updateservice]' || die "could not start updateservice"
+  echo "systest ---> Sleeping 3"
+  sleep 3 || die "sleep 3"
 }
 
 function setSysVars () {
-  if [ "$1" == "payara" ]; then
-    echo "systest ---> Running in payara mode: "
-    export SYSTEST_PATH="docker/deployments/systemtests-payara"
-    export COMPOSE_PROJECT_NAME=systemtestspayara
-    export systest="-payara"
-  else
-    echo "systest ---> Running in glassfish mode: "
-    export SYSTEST_PATH="docker/deployments/systemtests"
-    export COMPOSE_PROJECT_NAME=systemtests
-    export systest="-glassfish"
-  fi
-  
+  echo "systest ---> Setting systest variables"
+  export SYSTEST_PATH="docker/deployments/systemtests-payara"
+  export COMPOSE_PROJECT_NAME=systemtestspayara
   export HOST_IP=$(ip addr show | grep -A 99 '^2' | grep inet | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' |grep -v '^127.0.0.1' | head -1)
   echo "systest ---> Using host IP: ${HOST_IP}"
 }
 function main ()  {
-  echo "Setting systest variables"
   setSysVars ${1}
-  echo "systest ---> Setting up logdirs"
   setupLogAndLogdir
   echo "systest ---> systest ---> Stop glassfish containers"
   docker-compose down
-  echo "systest ---> Removing old images"
   removeImages
-  echo "systest ---> Starting containers"
   startContainers
-  echo "systest ---> retagging and removing containers"
   reTagAndRemove
-  echo "systest ---> waiting on containers"
   waitForOk
-  echo "systest ---> Sleeping 3"
-  sleep 3 || die "sleep 3"
   echo "systest ---> Start and run systemtests"
   docker-compose up ocb-tools-systemtests || die "docker-compose up ocb-tools-systemtests"
-  echo "systest ---> Collect log files"
   collect_logs
   sleep 3 || die "sleep 3"
   echo "systest ---> Stop glassfish containers"
