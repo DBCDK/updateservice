4a0dba615a947f749ddc50e73a76942c1a4bf35e
Anders Thrane Michelsen <atm@dbc.dk>
2018-03-19 10:49:00 +0100

    Add license header
    License header was missing from in some of the new files.
    Also removed unused code.

--- src/main/java/dk/dbc/updateservice/client/BibliographicRecordFactory.java
+++ src/main/java/dk/dbc/updateservice/client/BibliographicRecordFactory.java
@@ -1,25 +1,23 @@
+/*
+ * Copyright Dansk Bibliotekscenter a/s. Licensed under GNU GPL v3
+ *  See license text at https://opensource.dbc.dk/licenses/gpl-3.0
+ */
+
 package dk.dbc.updateservice.client;
 
 import dk.dbc.common.records.MarcConverter;
 import dk.dbc.common.records.MarcRecord;
-import dk.dbc.common.records.MarcRecordFactory;
 import dk.dbc.updateservice.service.api.BibliographicRecord;
 import dk.dbc.updateservice.service.api.ExtraRecordData;
 import dk.dbc.updateservice.service.api.RecordData;
-import dk.dbc.updateservice.utils.IOUtils;
 import org.slf4j.ext.XLogger;
 import org.slf4j.ext.XLoggerFactory;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
 import javax.xml.bind.JAXBException;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Factory class to construct BibliographicRecord instances.
@@ -52,179 +50,6 @@ import java.io.InputStream;
  */
 public class BibliographicRecordFactory {
     private static final XLogger logger = XLoggerFactory.getXLogger(BibliographicRecordFactory.class);
-
-    /**
-     * Constructs a BibliographicRecord from a XML resource with no extra record data.
-     * <p>
-     * The resource is assumed to be a marcxchange record.
-     * </p>
-     *
-     * @param resourceName Resource name.
-     * @return A new BibliographicRecord with the record from the the resource encoded as marxxchange.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadResource(String resourceName) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadResource(resourceName, null);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from an InputStream with no extra record data.
-     * <p>
-     * The input stream is assumed to be a marcxchange record.
-     * </p>
-     *
-     * @param in Input Stream.
-     * @return A new BibliographicRecord with the record from the the resource encoded as marxxchange.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadResource(InputStream in) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadResource(in, null);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from a XML resource with extra record data.
-     * <p>
-     * The resource is assumed to be a marcxchange record.
-     * </p>
-     *
-     * @param resourceName Resource name.
-     * @param data         Extra record data.
-     * @return A new BibliographicRecord with the record from the the resource encoded as marxxchange.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadResource(String resourceName, BibliographicRecordExtraData data) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadResource(BibliographicRecordFactory.class.getResourceAsStream(resourceName), data);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from a XML resource with extra record data.
-     * <p>
-     * The resource is assumed to be a marcxchange record.
-     * </p>
-     *
-     * @param in   Input Stream.
-     * @param data Extra record data.
-     * @return A new BibliographicRecord with the record from the the resource encoded as marxxchange.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadResource(InputStream in, BibliographicRecordExtraData data) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        DocumentBuilderFactory xmlFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder xmlBuilder = xmlFactory.newDocumentBuilder();
-        Document doc = xmlBuilder.parse(in);
-
-        BibliographicRecord record = new BibliographicRecord();
-        record.setRecordSchema("info:lc/xmlns/marcxchange-v1");
-        record.setRecordPacking("xml");
-
-        RecordData recData = new RecordData();
-        recData.getContent().add("\n");
-        recData.getContent().add(doc.getDocumentElement());
-        recData.getContent().add("\n");
-        record.setRecordData(recData);
-
-        record.setExtraRecordData(createExtraRecordData(data));
-        return record;
-    }
-
-    /**
-     * Constructs a BibliographicRecord from a resource of a record in line format.
-     * <p>
-     * The resource is assumed to be in line format of a record.
-     * </p>
-     *
-     * @param resourceName Resource name.
-     * @return A new BibliographicRecord with the record from the the resource encoded in line format.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadMarcRecordInLineFormat(String resourceName) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadMarcRecordInLineFormat(resourceName, null);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from a resource of a record in line format with extra record data.
-     * <p>
-     * The resource is assumed to be in line format of a record.
-     * </p>
-     *
-     * @param resourceName Resource name.
-     * @param data         Extra record data.
-     * @return A new BibliographicRecord with the record from the the resource encoded in line format.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadMarcRecordInLineFormat(String resourceName, BibliographicRecordExtraData data) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadMarcRecordInLineFormat(BibliographicRecordFactory.class.getResourceAsStream(resourceName), data);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from a file of a record in line format.
-     * <p>
-     * The resource is assumed to be in line format of a record.
-     * </p>
-     *
-     * @param file The file to use.
-     * @return A new BibliographicRecord with the record from the the resource encoded in line format.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadMarcRecordInLineFormat(File file) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadMarcRecordInLineFormat(new FileInputStream(file), null);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from a file of a record in line format with extra record data.
-     * <p>
-     * The resource is assumed to be in line format of a record.
-     * </p>
-     *
-     * @param file The file to use.
-     * @param data Extra record data.
-     * @return A new BibliographicRecord with the record from the the resource encoded in line format.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadMarcRecordInLineFormat(File file, BibliographicRecordExtraData data) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return loadMarcRecordInLineFormat(new FileInputStream(file), data);
-    }
-
-    /**
-     * Constructs a BibliographicRecord from an InputStream of a record in line format with extra record data.
-     * <p>
-     * The input stream is assumed to be in line format of a record.
-     * </p>
-     *
-     * @param inputStream Input Stream.
-     * @param data        Extra record data.
-     * @return A new BibliographicRecord with the record from the the resource encoded in line format.
-     * @throws ParserConfigurationException In case of parse error on the xml resource.
-     * @throws SAXException                 In case of sax error on the xml resource.
-     * @throws IOException                  In case of IO error on the xml resource.
-     * @throws JAXBException                In case of an JAXB error on the xml resource.
-     */
-    public static BibliographicRecord loadMarcRecordInLineFormat(InputStream inputStream, BibliographicRecordExtraData data) throws ParserConfigurationException, SAXException, IOException, JAXBException {
-        return newMarcRecord(MarcRecordFactory.readRecord(IOUtils.readAll(inputStream, "UTF-8")), data);
-    }
 
     /**
      * Constructs a BibliographicRecord from a MarcRecord with no extra record data.
