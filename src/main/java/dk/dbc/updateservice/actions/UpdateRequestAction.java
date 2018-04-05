f8ad35964bfbe093524db190a80dd2e261696e69
Anders Thrane Michelsen <atm@dbc.dk>
2018-04-05 07:57:33 +0200

    Add more debug logging

--- src/main/java/dk/dbc/updateservice/actions/UpdateRequestAction.java
+++ src/main/java/dk/dbc/updateservice/actions/UpdateRequestAction.java
@@ -167,7 +167,12 @@ public class UpdateRequestAction extends AbstractAction {
         logger.info("Record schema: {}", state.getUpdateServiceRequestDTO().getBibliographicRecordDTO() != null ?
                 state.getUpdateServiceRequestDTO().getBibliographicRecordDTO().getRecordSchema() : "Unknown");
         logger.info("Request record: \n{}", state.readRecord());
-        logger.info("Record schema: {}", state.getUpdateServiceRequestDTO().getBibliographicRecordDTO().getRecordSchema());
+        if (state.getUpdateServiceRequestDTO().getBibliographicRecordDTO() != null) {
+            // UpdateServiceRequestDTO will always be there but BibliographicRecordDTO might be null
+            // If BibliographicRecordDTO is null the request should fail with a nice validation error
+            // The request shouldn't fail because a null pointer exception was thrown while logging
+            logger.info("Record schema: {}", state.getUpdateServiceRequestDTO().getBibliographicRecordDTO().getRecordSchema());
+        }
         logger.info("======================================");
     }
 
