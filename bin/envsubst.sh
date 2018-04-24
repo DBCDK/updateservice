ee294576ec3527f352341fdd46ab7c25c1a8f601
Anders Thrane Michelsen <atm@dbc.dk>
2018-04-24 09:39:40 +0200

    Remove 'set -e' again
    Well crap, it seems like 'set -e' is enabled for the whole execution
    process which means the systemtest script fails because of not-relevant
    errors.

--- bin/envsubst.sh
+++ bin/envsubst.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-set -e
 
 envsubst '${SUBST_IMAGE_NAME}' < docker/deployments/systemtests-payara/docker-compose.yml > docker/deployments/systemtests-payara/docker-compose.tmp
 mv docker/deployments/systemtests-payara/docker-compose.tmp docker/deployments/systemtests-payara/docker-compose.yml
