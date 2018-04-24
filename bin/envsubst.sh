40cbee35d8d779177168ef388033c047b1907cda
Anders Thrane Michelsen <atm@dbc.dk>
2018-04-24 08:53:08 +0200

    Clean up envsubst script
    One file no longer need to be envsubsted so that was removed.
    In addition 'set -e' was added for better error handling.

--- bin/envsubst.sh
+++ bin/envsubst.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
-
-export SUBST_IMAGE_NAME=$1
-envsubst '${SUBST_IMAGE_NAME}' < system-test.sh > system-test.tmp
-mv system-test.tmp system-test.sh
-chmod +x system-test.sh
+set -e
 
 envsubst '${SUBST_IMAGE_NAME}' < docker/deployments/systemtests-payara/docker-compose.yml > docker/deployments/systemtests-payara/docker-compose.tmp
 mv docker/deployments/systemtests-payara/docker-compose.tmp docker/deployments/systemtests-payara/docker-compose.yml
