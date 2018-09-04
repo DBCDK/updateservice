2a4a597175c9da615f8936cfcb05d21858bf15c5
Anders Thrane Michelsen <atm@dbc.dk>
2018-09-04 07:50:45 +0200

    Add howru service to updateservice
    We need an endpoint which can tell us the status of updateservice and
    other services which updateservice depends on for use in a Zabbix probe.

--- src/main/java/dk/dbc/updateservice/rest/ApplicationConfig.java
+++ src/main/java/dk/dbc/updateservice/rest/ApplicationConfig.java
@@ -6,6 +6,8 @@
 package dk.dbc.updateservice.rest;
 
 import javax.ws.rs.core.Application;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -13,20 +15,10 @@ import java.util.Set;
  */
 @javax.ws.rs.ApplicationPath("rest")
 public class ApplicationConfig extends Application {
+    private static final Set<Class<?>> classes = new HashSet<>(Arrays.asList(StatusService.class, HowruService.class));
+
     @Override
     public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(StatusService.class);
+        return classes;
     }
 }
