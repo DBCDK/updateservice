bc411724c87abff8836827559b31e0bf618505b6
Anders Thrane Michelsen <atm@dbc.dk>
2019-07-17 09:29:13 +0200

    Add littolk agency constant

--- src/main/java/dk/dbc/updateservice/update/RawRepo.java
+++ src/main/java/dk/dbc/updateservice/update/RawRepo.java
@@ -52,6 +52,7 @@ public class RawRepo {
     private static final XLogger logger = XLoggerFactory.getXLogger(RawRepo.class);
     public static final int COMMON_AGENCY = 870970;
     public static final int ARTICLE_AGENCY = 870971;
+    public static final int LITTOLK_AGENCY = 870974;
     public static final int AUTHORITY_AGENCY = 870979;
     public static final List<String> DBC_AGENCY_LIST = Arrays.asList("870970", "870971", "870973", "870974", "870975", "870976", "870978", "870979", "190002", "190004", "190007", "190008");
     public static final List<String> DBC_AGENCY_ALL = Arrays.asList("190002", "190004", "191919", "870970", "870971", "870974", "870979"); // More will probably be added later
