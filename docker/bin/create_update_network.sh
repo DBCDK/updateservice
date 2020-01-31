2f2f890ce8e4cfd244365fa5a7767d6209263e5e
atmdbc <atm@dbc.dk>
2020-01-31 12:59:48 +0100

    Change docker network ip range

--- docker/bin/create_update_network.sh
+++ docker/bin/create_update_network.sh
@@ -3,4 +3,4 @@
 # This script are only needed to be run once on a particular machine.
 # theory says that the network will survive a reboot
 #
-docker network create --subnet=192.180.0.0/22 update-compose-network
+docker network create --subnet=192.168.42.1/24 update-compose-network
