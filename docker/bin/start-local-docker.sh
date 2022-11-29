985db9886a38f46c2d5d343cfca45908df2ca065
Jens Grønborg Andersen <jega@dbc.dk>
2022-11-29 16:59:23 +0100

    Fixing scripts and stuff

--- docker/bin/start-local-docker.sh
+++ docker/bin/start-local-docker.sh
@@ -13,18 +13,18 @@ then
     echo "Found testrun.properties so starting"
 else
     echo "ERROR!"
-    echo "~/.ocb-tools/testrun.properties must exist in order to run this script, but the file is missing."
+    echo "${TEST_RUN} must exist in order to run this script, but the file is missing."
     echo "A possible cause for this is that start_dev_docker.sh has not been executed yet."
     exit 1
 fi
 
-export SOLR_PORT_NR=$(grep solr.port ${HOME}/.ocb-tools/testrun.properties | awk '{print $3}')
+export SOLR_PORT_NR=$(readOcbProperty solr.port)
 DOCKER_FOLDER=${IDEA_ROOT}/docker/update-payara-dev
 
-export HOST_IP=$(getHostIP)
+export HOST_IP="$(getHostIP)"
 echo "HOST_IP: $HOST_IP"
 
-cd ${IDEA_ROOT}
+cd ${IDEA_ROOT} || fail "Failed to cd into ${IDEA_ROOT}"
 mvn verify install -Dmaven.test.skip=true
 
 rm "${DOCKER_FOLDER}/*.war"
@@ -32,22 +32,17 @@ cp ${IDEA_ROOT}/target/updateservice-2.0-SNAPSHOT.war ${DOCKER_FOLDER}
 
 docker build -t docker-metascrum.artifacts.dbccloud.dk/update-payara-dev:latest ${DOCKER_FOLDER}
 
-cd docker/deployments/dev
+cd docker/deployments/dev || fail "Failed to cd into docker/deployments/dev"
 
 export COMPOSE_PROJECT_NAME=${USER}
 
-DEV_NUMBERROLL_URL=${DEV_NUMBERROLL_URL:-NOTSET}
-if [ ${DEV_NUMBERROLL_URL} = "NOTSET" ]
-then
-    export DEV_NUMBERROLL_URL="http://${HOST_IP}:${SOLR_PORT_NR}"
-fi
-
-DEV_VIPCORE_ENDPOINT="${DEV_VIPCORE_ENDPOINT:-http://${HOST_IP}:${SOLR_PORT_NR}}"
-DEV_IDP_SERVICE_URL="${DEV_IDP_SERVICE_URL:-http://${HOST_IP}:${SOLR_PORT_NR}}"
+export DEV_NUMBERROLL_URL="${DEV_NUMBERROLL_URL:-http://${HOST_IP}:${SOLR_PORT_NR}}"
+export DEV_VIPCORE_ENDPOINT="${DEV_VIPCORE_ENDPOINT:-http://${HOST_IP}:${SOLR_PORT_NR}}"
+export DEV_IDP_SERVICE_URL="${DEV_IDP_SERVICE_URL:-http://${HOST_IP}:${SOLR_PORT_NR}}"
 
 # Solr FBS settings
 export DEV_SOLR_ADDR="${DEV_SOLR_ADDR:-${HOST_IP}}"
-export DEV_SOLR_PORT=${SOLR_PORT_NR}:-""}
+export DEV_SOLR_PORT=${SOLR_PORT_NR:-""}
 export DEV_SOLR_PATH="${DEV_SOLR_PATH:-solr/raapost-index}"
 export DEV_SOLR_URL="http://${DEV_SOLR_ADDR}:${DEV_SOLR_PORT}/${DEV_SOLR_PATH}"
 
@@ -57,10 +52,10 @@ export DEV_SOLR_BASIS_PORT="${DEV_SOLR_BASIS_PORT:-${SOLR_PORT_NR}}"
 export DEV_SOLR_BASIS_PATH="${DEV_SOLR_BASIS_PATH:-solr/basis-index}"
 export DEV_SOLR_BASIS_URL="http://${DEV_SOLR_BASIS_ADDR}:${DEV_SOLR_BASIS_PORT}/${DEV_SOLR_BASIS_PATH}"
 
-export DEV_RAWREPO_DB_URL=$(grep rawrepo.db.url ${TEST_RUN} | awk '{print $3}')
-export DEV_UPDATE_DB_URL=$(grep updateservice.db.url ${TEST_RUN} | awk '{print $3}')
-export DEV_OPENCAT_BUSINESS_URL=$(grep opencat.business.url ${TEST_RUN} | awk '{print $3}')
-export DEV_HOLDINGS_URL=$(grep holdings.url ${TEST_RUN} | awk '{print $3}')
+export DEV_RAWREPO_DB_URL=$(readOcbProperty rawrepo.db.url)
+export DEV_UPDATE_DB_URL=$(readOcbProperty updateservice.db.url)
+export DEV_OPENCAT_BUSINESS_URL=$(readOcbProperty opencat.business.url)
+export DEV_HOLDINGS_URL=$(readOcbProperty holdings.url)
 
 echo -e "Rawrepo db : ${DEV_RAWREPO_DB_URL}"
 echo -e "Updateservice db : ${DEV_RAWREPO_DB_URL}"
@@ -84,7 +79,7 @@ export BUILD_SERVICE_URL="http://${HOST_IP}:${UPDATESERVICE_PORT_8080}/UpdateSer
 
 ${DOCKER_COMPOSE_CMD} up -d updateservice-facade
 
-UPDATESERVICE_FACADE_IMAGE=`${DOCKER_COMPOSE_CMD} ps -q updateservice-facade`
+UPDATESERVICE_FACADE_IMAGE=$(${DOCKER_COMPOSE_CMD} ps -q updateservice-facade)
 UPDATESERVICE_FACADE_PORT_8080=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${UPDATESERVICE_FACADE_IMAGE} `
 echo -e "UPDATESERVICE_FACADE_PORT_8080 is ${UPDATESERVICE_FACADE_PORT_8080}\n"
 UPDATESERVICE_FACADE_PORT_8686=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8686/tcp") 0).HostPort}}' ${UPDATESERVICE_FACADE_IMAGE} `
@@ -96,5 +91,5 @@ echo -e "UPDATESERVICE_FACADE_PORT_4848 is ${UPDATESERVICE_FACADE_PORT_4848}\n"
 "${DOCKER_BIN}/healthcheck-update-facade-service.sh" ${HOST_IP} ${UPDATESERVICE_FACADE_PORT_8080} 220 || fail "could not start update-facade-service"
 cd "${PWD}"
 
-sed -i -e "/^buildservice.url/s/^.*$/buildservice.url = http:\/\/${HOST_IP}:${UPDATESERVICE_FACADE_PORT_8080}/" ${HOME}/.ocb-tools/testrun.properties
-sed -i -e "/^updateservice.url/s/^.*$/updateservice.url = http:\/\/${HOST_IP}:${UPDATESERVICE_FACADE_PORT_8080}/" ${HOME}/.ocb-tools/testrun.properties
+sed -i -e "/^buildservice.url/s/^.*$/buildservice.url = http:\/\/${HOST_IP}:${UPDATESERVICE_FACADE_PORT_8080}/" "${TEST_RUN}"
+sed -i -e "/^updateservice.url/s/^.*$/updateservice.url = http:\/\/${HOST_IP}:${UPDATESERVICE_FACADE_PORT_8080}/" "${TEST_RUN}"
--- docker/bin/start_dev_docker.sh
+++ docker/bin/start_dev_docker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -x
+#set -x
 
 # If this is set different from N then also change
 # image: "${METASCRUM_REPO}"/update-payara:latest
@@ -21,7 +21,6 @@ DOCKER_COMPOSE_CMD="$(command -v docker-compose > /dev/null && echo docker-compo
 
 RAWREPO_VERSION=1.15-snapshot
 RAWREPO_DIT_TAG=DIT-5165
-HOLDINGS_ITEMS_VERSION=1.3
 OPENCAT_BUSINESS_SERVICE_TAG=latest
 RAWREPO_RECORD_SERVICE_TAG=DIT-330
 
@@ -30,7 +29,7 @@ cd ${IDEA_ROOT}/docker
 docker build -t "${METASCRUM_REPO}"/update-payara:latest update-payara \
  || fail "Could not create ${IDEA_ROOT}/docker/logs/update/app ${IDEA_ROOT}/docker/logs/update/server"
 
-cd ${IDEA_ROOT}/docker/deployments/dev
+cd ${IDEA_ROOT}/docker/deployments/dev || fail "Failed to cd into docker/deployments/dev"
 
 if [ ! -d "${OCB_TOOLS}" ]
 then
@@ -49,52 +48,53 @@ ${DOCKER_COMPOSE_CMD} down
 ${DOCKER_COMPOSE_CMD} ps
 echo "docker ps : $?"
 
-docker rmi -f "${METASCRUM_REPO}"/rawrepo-postgres-${RAWREPO_VERSION}:"${USER}"
-docker rmi -f "${METASCRUM_REPO}"/update-postgres:"${USER}"
-docker rmi -f "${METASCRUM_REPO}"/opencat-business:"${USER}"
-docker rmi -f "${METASCRUM_REPO}"/rawrepo-record-service:"${USER}"
+docker rmi -f "${METASCRUM_REPO}/rawrepo-postgres-${RAWREPO_VERSION}:${USER}"
+docker rmi -f "${METASCRUM_REPO}/update-postgres:${USER}"
+docker rmi -f "${METASCRUM_REPO}/opencat-business:${USER}"
+docker rmi -f "${METASCRUM_REPO}/rawrepo-record-service:${USER}"
 if [ "$USE_LOCAL_PAYARA" = "N" ]
 then
-    docker rmi -f "${METASCRUM_REPO}"/update-payara:"${USER}"
+    docker rmi -f "${METASCRUM_REPO}/update-payara:${USER}"
 fi
 ${DOCKER_COMPOSE_CMD} pull
 ${DOCKER_COMPOSE_CMD} up -d rawrepoDb
 ${DOCKER_COMPOSE_CMD} up -d updateserviceDb
 ${DOCKER_COMPOSE_CMD} up -d fakeSmtp
 
-docker tag "${METASCRUM_REPO}"/rawrepo-postgres-${RAWREPO_VERSION}:${RAWREPO_DIT_TAG} "${METASCRUM_REPO}"/rawrepo-postgres-${RAWREPO_VERSION}:"${USER}"
-docker rmi "${METASCRUM_REPO}"/rawrepo-postgres-${RAWREPO_VERSION}:${RAWREPO_DIT_TAG}
-docker tag "${METASCRUM_REPO}"/update-postgres:latest "${METASCRUM_REPO}"/update-postgres:"${USER}"
-docker rmi "${METASCRUM_REPO}"/update-postgres:latest
+docker tag "${METASCRUM_REPO}/rawrepo-postgres-${RAWREPO_VERSION}:${RAWREPO_DIT_TAG}" "${METASCRUM_REPO}/rawrepo-postgres-${RAWREPO_VERSION}:${USER}"
+docker rmi "${METASCRUM_REPO}/rawrepo-postgres-${RAWREPO_VERSION}:${RAWREPO_DIT_TAG}"
+docker tag "${METASCRUM_REPO}/update-postgres:latest" "${METASCRUM_REPO}/update-postgres:${USER}"
+docker rmi "${METASCRUM_REPO}/update-postgres:latest"
 
-RAWREPO_IMAGE=`${DOCKER_COMPOSE_CMD} ps -q rawrepoDb`
-export RAWREPO_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "5432/tcp") 0).HostPort}}' ${RAWREPO_IMAGE} `
+RAWREPO_IMAGE=$(${DOCKER_COMPOSE_CMD} ps -q rawrepoDb)
+export RAWREPO_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "5432/tcp") 0).HostPort}}' ${RAWREPO_IMAGE})
 echo -e "RAWREPO_PORT is $RAWREPO_PORT\n"
 
 UPDATESERVICEDB_IMAGE=$(${DOCKER_COMPOSE_CMD} ps -q updateserviceDb)
 export UPDATESERVICEDB_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "5432/tcp") 0).HostPort}}' ${UPDATESERVICEDB_IMAGE})
 echo -e "UPDATESERVICEDB_PORT is $UPDATESERVICEDB_PORT\n"
 
-echo "rawrepo.jdbc.driver = org.postgresql.Driver" > "${TEST_RUN}"
-echo "rawrepo.jdbc.conn.url = jdbc:postgresql://${HOST_IP}:${RAWREPO_PORT}/rawrepo" >> "${TEST_RUN}"
-echo "rawrepo.jdbc.conn.user = rawrepo" >> "${TEST_RUN}"
-echo "rawrepo.jdbc.conn.passwd = thePassword" >> "${TEST_RUN}"
-echo "rawrepo.db.url = rawrepo:thePassword@${HOST_IP}:${RAWREPO_PORT}/rawrepo" >> "${TEST_RUN}"
+clearOcbProperties
+appendOcbProperty "rawrepo.jdbc.driver = org.postgresql.Driver"
+appendOcbProperty "rawrepo.jdbc.conn.url = jdbc:postgresql://${HOST_IP}:${RAWREPO_PORT}/rawrepo"
+appendOcbProperty "rawrepo.jdbc.conn.user = rawrepo"
+appendOcbProperty "rawrepo.jdbc.conn.passwd = thePassword"
+appendOcbProperty "rawrepo.db.url = rawrepo:thePassword@${HOST_IP}:${RAWREPO_PORT}/rawrepo"
 
-echo "updateservice.db.url = updateservice:thePassword@${HOST_IP}:${UPDATESERVICEDB_PORT}/updateservice" >> "${TEST_RUN}"
+appendOcbProperty "updateservice.db.url = updateservice:thePassword@${HOST_IP}:${UPDATESERVICEDB_PORT}/updateservice"
 
-echo "solr.port = ${SOLR_PORT}" >> "${TEST_RUN}"
-echo "holdings.port = ${HOLDINGS_PORT}" >> "${TEST_RUN}"
-export DEV_HOLDINGS_URL="http://localhost:${HOLDINGS_PORT}/api"
-echo "holdings.url" = ${DEV_HOLDINGS_URL} >> "${TEST_RUN}"
+appendOcbProperty "solr.port = ${SOLR_PORT}"
+appendOcbProperty "holdings.port = ${HOLDINGS_PORT}"
+export DEV_HOLDINGS_URL="http://${HOST_IP}:${HOLDINGS_PORT}/api"
+appendOcbProperty "holdings.url = ${DEV_HOLDINGS_URL}"
 
 #Set x.forwarded.for to the ip of devel8. This way it is possible to run the test behind the vpn
-echo "request.headers.x.forwarded.for = 172.17.20.165" >> "${TEST_RUN}"
+appendOcbProperty "request.headers.x.forwarded.for = 172.17.20.165"
 
-echo "rawrepo.provider.name.dbc = dataio-update" >> "${TEST_RUN}"
-echo "rawrepo.provider.name.fbs = opencataloging-update" >> "${TEST_RUN}"
-echo "rawrepo.provider.name.ph = fbs-ph-update" >> "${TEST_RUN}"
-echo "rawrepo.provider.name.ph.holdings = dataio-ph-holding-update" >> "${TEST_RUN}"
+appendOcbProperty "rawrepo.provider.name.dbc = dataio-update"
+appendOcbProperty "rawrepo.provider.name.fbs = opencataloging-update"
+appendOcbProperty "rawrepo.provider.name.ph = fbs-ph-update"
+appendOcbProperty "rawrepo.provider.name.ph.holdings = dataio-ph-holding-update"
 
 export DEV_VIPCORE_ENDPOINT="http://${HOST_IP}:${SOLR_PORT}"
 export DEV_NUMBERROLL_URL="http://${HOST_IP}:${SOLR_PORT}"
@@ -102,20 +102,20 @@ export DEV_RAWREPO_DB_URL="rawrepo:thePassword@${HOST_IP}:${RAWREPO_PORT}/rawrep
 
 #opencat-business-service
 ${DOCKER_COMPOSE_CMD} up -d rawrepo-record-service
-RAWREPO_RECORD_SERVICE_CONTAINER=`${DOCKER_COMPOSE_CMD} ps -q rawrepo-record-service`
-export RAWREPO_RECORD_SERVICE_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${RAWREPO_RECORD_SERVICE_CONTAINER} `
+RAWREPO_RECORD_SERVICE_CONTAINER=$(${DOCKER_COMPOSE_CMD} ps -q rawrepo-record-service)
+export RAWREPO_RECORD_SERVICE_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${RAWREPO_RECORD_SERVICE_CONTAINER})
 echo -e "RAWREPO_RECORD_SERVICE_PORT is ${RAWREPO_RECORD_SERVICE_PORT}\n"
-echo "rawrepo.record.service.url = http://${HOST_IP}:${RAWREPO_RECORD_SERVICE_PORT}" >> "${TEST_RUN}"
+appendOcbProperty "rawrepo.record.service.url = http://${HOST_IP}:${RAWREPO_RECORD_SERVICE_PORT}"
 
 export DEV_RAWREPO_RECORD_SERVICE_URL="http://${HOST_IP}:${RAWREPO_RECORD_SERVICE_PORT}"
 export DEV_SOLR_URL="http://${HOST_IP}:${SOLR_PORT}/solr/raapost-index"
 export DEV_SOLR_BASIS_URL="http://${HOST_IP}:${SOLR_PORT}/solr/basis-index"
 
 ${DOCKER_COMPOSE_CMD} up -d opencat-business-service
-OPENCAT_BUSINESS_SERVICE_CONTAINER=`${DOCKER_COMPOSE_CMD} ps -q opencat-business-service`
-export OPENCAT_BUSINESS_SERVICE_PORT=`docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${OPENCAT_BUSINESS_SERVICE_CONTAINER} `
+OPENCAT_BUSINESS_SERVICE_CONTAINER=$(${DOCKER_COMPOSE_CMD} ps -q opencat-business-service)
+export OPENCAT_BUSINESS_SERVICE_PORT=$(docker inspect --format='{{(index (index .NetworkSettings.Ports "8080/tcp") 0).HostPort}}' ${OPENCAT_BUSINESS_SERVICE_CONTAINER})
 echo -e "OPENCAT_BUSINESS_SERVICE_PORT is ${OPENCAT_BUSINESS_SERVICE_PORT}\n"
-echo "opencat.business.url = http://${HOST_IP}:${OPENCAT_BUSINESS_SERVICE_PORT}" >> "${TEST_RUN}"
+appendOcbProperty "opencat.business.url = http://${HOST_IP}:${OPENCAT_BUSINESS_SERVICE_PORT}"
 
 docker tag "${METASCRUM_REPO}"/opencat-business:${OPENCAT_BUSINESS_SERVICE_TAG} "${METASCRUM_REPO}"/opencat-business:${USER}
 docker rmi "${METASCRUM_REPO}"/opencat-business:${OPENCAT_BUSINESS_SERVICE_TAG}
@@ -123,8 +123,8 @@ docker tag "${METASCRUM_REPO}"/rawrepo-record-service:${RAWREPO_RECORD_SERVICE_T
 docker rmi "${METASCRUM_REPO}"/rawrepo-record-service:${RAWREPO_RECORD_SERVICE_TAG}
 
 #Look in start-local-docker.sh for final configuration
-echo "updateservice.url = dummy" >> "${TEST_RUN}"
-echo "buildservice.url = dummy" >> "${TEST_RUN}"
+appendOcbProperty "updateservice.url = dummy"
+appendOcbProperty "buildservice.url = dummy"
 
-"${DOCKER_BIN}/healthcheck-rawrepo-record-service.sh" ${HOST_IP} ${RAWREPO_RECORD_SERVICE_PORT} 220 || die "could not start rawrepo-record-service"
-"${DOCKER_BIN}/healthcheck-opencat-business-service.sh" ${HOST_IP} ${OPENCAT_BUSINESS_SERVICE_PORT} 220 || die "could not start opencat-business-service"
+"${DOCKER_BIN}/healthcheck-rawrepo-record-service.sh" ${HOST_IP} ${RAWREPO_RECORD_SERVICE_PORT} 220 || fail "could not start rawrepo-record-service"
+"${DOCKER_BIN}/healthcheck-opencat-business-service.sh" ${HOST_IP} ${OPENCAT_BUSINESS_SERVICE_PORT} 220 || fail "could not start opencat-business-service"
